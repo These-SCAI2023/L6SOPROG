267f4bcc6b623f9e4da35a768efec97d931d76fe
These-SCAI2023 <caroline.parfait@sorbonne-universite.fr>
2024-02-16 12:54:32 +0100

    TD/code/TD1_CKP

---
+++ TD/code/TD1a_CKP.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 16 10:14:00 2024
+
+@author: ceres
+"""
+import glob
+from TD1_CKP import *
+
+liste_fichiers=[]
+c=1
+liste_corpus=["appr","test"]
+path=f"./corpus_multi/*/{liste_corpus[c]}/*"
+
+for fichier in glob.glob(path):
+    liste_fichiers.append(fichier) 
+print("Nombre de fichiers : %i"%len(liste_fichiers))
+
+
+dic_langues = get_dic_langues(liste_fichiers)
+print(dic_langues.keys())
+
+
+for NB_mots in [10, 20, 30, 43]:
+    dic_modeles = get_model(dic_langues, NB_mots)
+    stocker_json(f"models/{liste_corpus[c]}_models_3gram_%i.json"%NB_mots,dic_modeles)
